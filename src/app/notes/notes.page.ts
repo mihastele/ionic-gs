0394dbea9c6df246573aad643fbde901b9526498
Miha Stele <stele1000@gmail.com>
2021-10-27 21:28:32 +0200

    Add simple list

--- src/app/notes/notes.page.ts
+++ src/app/notes/notes.page.ts
@@ -1,11 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { Note } from './note.model';
+
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.page.html',
   styleUrls: ['./notes.page.scss'],
 })
 export class NotesPage implements OnInit {
+
+  notes: Note[] = [{
+    id: 'aw13w',
+    title: 'Aaaaa',
+    body: '1123k1č4mjklmlkmalmascv.das md',
+    attachmentUrls: []
+  },
+
+  {
+    id: 'a22',
+    title: 'Body bvuilding',
+    body: `1123k1č4mjklmlkmalmascv.da
+    asad
+    asdasd
+    asd
+    dass md`,
+    attachmentUrls: ['./seee']
+  },
+
+  {
+    id: 'aw1332w',
+    title: 'BOSMVB',
+    body: '1123k1č4mjklmlkmalmascasfafsasfavasfasfasff',
+    attachmentUrls: []
+  },
+  {
+    id: 'aw1323w',
+    title: 'Čokolino',
+    body: '1123k1č4mjkasfafasfsafasfasas md',
+    attachmentUrls: []
+  }
+
+];
 
   constructor() { }
 
