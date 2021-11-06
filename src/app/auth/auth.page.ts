a51cac3382454e776f06b98ec8b39b21a16d0941
Miha Stele <stele1000@gmail.com>
2021-11-06 20:41:09 +0100

    Tutorial 169. complete

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -36,4 +37,6 @@ export class AuthPage implements OnInit {
         }, 1000);
       });
   }
+
+  onSumbit(form: NgForm) {}
 }
