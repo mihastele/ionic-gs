ced366fb466fc1571be71209452b71c68fd76521
Miha Stele <stele1000@gmail.com>
2021-11-07 21:02:51 +0100

    finish 170. tutorial

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -11,6 +11,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -36,6 +37,10 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1000);
       });
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
   onSumbit(form: NgForm) {}
